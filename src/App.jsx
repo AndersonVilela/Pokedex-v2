1f4385f54b4a60dd72cf830b991df4d2390d3528
Anderson Vilela <andersonvilela.al@gmailcom>
2022-09-17 23:24:21 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,38 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Pokecard from './components/Pokecard';
 import axios from 'axios';
+import { Grid } from './components/Grid/Grid';
+
 
 
 
 function App() {
 
-  const [pokemons, setPokemons] = useState('');
-  const [listPokemons, setListPokemons] = useState([])
-  for (let i = 1; i <= 10; i++) {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/1`)
-      .then((res) => setPokemons(res.data.name))
-    listPokemons.push(pokemons)
+  const [pokemons, setPokemons] = useState([]);
 
+  useEffect(() => {
+    getPokemons();
+  }, [])
+
+  function getPokemons() {
+    let endpoints = [];
+    for (let i = 1; i <= 24; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+    }
+    let response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res))
   }
 
-  console.log(listPokemons)
 
   return (
     <div >
-
       <Navbar />
-      <Pokecard name='bulbasaur' />
-
+      <Grid>
+        {pokemons.map((pokemon, key) => (
+          <Pokecard name={pokemon.data.name} avatar={pokemon.data.name} key={key} types={pokemon.data.types} />
+        ))}
+      </Grid>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Pokecard/index.jsx
+++ src/components/Pokecard/index.jsx
@@ -1,20 +1,32 @@
 import { Container, PokeAvatar, PokeName, PokeType, SecLeft, SecRight, TypePoke } from "./style";
+import React from "react";
 
 
-function Pokecard({ name }) {
+function Pokecard({ name, avatar, types }) {
+
+  const setType = () => {
+    if (types[1]) {
+      return (
+        <>
+          <TypePoke>{types[0].type.name}</TypePoke>
+          <TypePoke>{types[1].type.name}</TypePoke>
+        </>
+      )
+    }
+    return <TypePoke>{types[0].type.name}</TypePoke>
+  }
 
   return (
     <Container  >
       <SecLeft>
         <PokeName>{name}</PokeName>
         <PokeType>
-          <TypePoke>Tipo 1</TypePoke>
-          <TypePoke>Tipo 2</TypePoke>
+          {setType()}
         </PokeType>
       </SecLeft>
 
       <SecRight>
-        <PokeAvatar src="https://www.pkparaiso.com/xy/sprites/animados/bulbasaur.gif" alt="Gif do pokemon" />
+        <PokeAvatar src={`http://www.pkparaiso.com/imagenes/xy/sprites/animados/${avatar}.gif`} alt="Gif do pokemon" />
       </SecRight>
     </Container>
   )
